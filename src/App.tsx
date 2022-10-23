402a57d1fb91ac45b12df4e4724f85addb5cdc98
garnio1987 <olegar2004@mail.ru>
2022-10-23 19:02:54 +0300

    fix: eslint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import React, { useContext } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import {
-  createTheme,
-  responsiveFontSizes,
-  ThemeProvider
-} from '@mui/material/styles'
-import { darkThemeProperties, lightThemeProperties } from './Theme'
-import { DarkModeContext } from './context/darkModeContext'
-import AppRouter from './components/routes/AppRouter'
+import React, { useContext } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
+import { darkThemeProperties, lightThemeProperties } from './Theme';
+import { DarkModeContext } from './context/darkModeContext';
+import AppRouter from './components/routes/AppRouter';
 
 const App = () => {
-  const { darkMode } = useContext(DarkModeContext)
+  const { darkMode } = useContext(DarkModeContext);
   const selectedTheme = darkMode
     ? responsiveFontSizes(createTheme(darkThemeProperties))
-    : responsiveFontSizes(createTheme(lightThemeProperties))
+    : responsiveFontSizes(createTheme(lightThemeProperties));
 
   return (
     <ThemeProvider theme={selectedTheme}>
+      s
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
